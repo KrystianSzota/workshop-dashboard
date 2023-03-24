93a2b7fc566a60f42e594d48581815e033179687
szotas <krystian.szota.it@gmail.com>
2023-03-24 09:28:47 +0100

    rest files

---
+++ src/main/java/com/example/workshopdashboard/WorkshopDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workshopdashboard/WorkshopDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
